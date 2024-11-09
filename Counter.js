78ca4eb263359775e5431d52eddd28c3d9226b3b
Athulya-das005 <athulyadas005@gmail.com>
2024-11-09 17:52:19 +0530

    Add files via upload

---
+++ Counter.js
@@ -0,0 +1,14 @@
+let createcounter=function(n){
+      return function(){
+           return n++
+    }
+}
+
+const counter=createcounter(10)
+console.log(counter() );
+console.log(counter() );
+console.log(counter() );
+console.log(counter() );
+console.log(counter() );
+
+
