07c43ef7a5666badede0d2e87b985219ce8da263
Athulya-das005 <athulyadas005@gmail.com>
2024-12-17 15:11:52 +0530

    Add files via upload

---
+++ SmallestEvenMultiple.js
@@ -0,0 +1,10 @@
+function smallestEvenMultiple(n){
+         if(n%2==0){
+            console.log(n);
+            
+         }else{
+            console.log(n*2);
+            
+         }
+}
+smallestEvenMultiple(6)
